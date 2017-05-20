273a2d362bb3eb62e8823846cd561269e3b84dcd
Khyojung <918fall@gmail.com>
2017-05-20 09:55:52 +0900

    message

--- init.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\hyojung\Desktop\»õ Æú´õ
-git init
-git remote add origin https://github.com/kdve/Sogra
--- sync.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd C:\Users\hyojung\Desktop\»õ Æú´õ
-git status
-git add --all
-git commit -m "%date% %time%"
-git pull
-git push origin master
